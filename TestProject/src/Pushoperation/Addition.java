efc4b2ba251a94992c8668d955daf18f9647695e
Ramezzraja <113331636+Ramezzraja@users.noreply.github.com>
2022-09-13 12:33:06 +0530

    first addition program

---
+++ TestProject/src/Pushoperation/Addition.java
@@ -0,0 +1,16 @@
+package Pushoperation;
+
+public class Addition
+{
+	public void add(int a,int b )
+	{
+	int c=a+b;
+	System.out.println(c);
+	}
+public static void main(String[]args)
+{
+	Addition d=new Addition();
+	d.add(20,30);
+	
+}
+}
